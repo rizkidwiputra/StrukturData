e38fb2609237f7b3c2d6f76f629e7d2920d137c7
PrasZ007 <212410103043@mail.unej.ac.id>
2022-09-18 11:08:21 +0700

    p

--- soal2/main.py
+++ soal2/main.py
@@ -1,14 +1,13 @@
 print("\n=====> Polindrom <=====\n")
 
-def palindrom(array,start, panjang):
+def palindrom(array, start, panjang):
     tengah = panjang//2
-    belakang =+1
     if tengah == start:
-        return "Palindrom"
-    elif array[start] != array[-belakang]:
+        return "--> Palindrom <--"
+    elif array[start] != array[-(start+1)]:
         return "Bukan Palindrom"
     else:
-        return palindrom(array, belakang, len(array))
+        return palindrom(array, start+1, len(array))
 
 bilangan = [1,2,3,4,4,3,2,1]
 
